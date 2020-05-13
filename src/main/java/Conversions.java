1ea6f27138b90a26c6932db42850b0a90c8c6735
NGCollen <collenn@nedbank.co.za>
2020-05-13 20:18:55 +0200

    ktoc

--- src/main/java/Conversions.java
+++ src/main/java/Conversions.java
@@ -1,3 +1,4 @@
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -5,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Conversions {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/ktoc")
-    public double KelvinToCelsius(double kelvin){
+    @RequestMapping(method = RequestMethod.GET, path = "/ktoc/{kelvin}")
+    public double KelvinToCelsius(@PathVariable double kelvin){
         double celsius = kelvin - 273.15;
         return celsius;
     }
