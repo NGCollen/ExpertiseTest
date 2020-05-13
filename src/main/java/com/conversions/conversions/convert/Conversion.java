8cb342b40d32764223b6cebcafb1d3402e09b2f4
NGCollen <collenn@nedbank.co.za>
2020-05-14 00:38:05 +0200

    Conversion

--- src/main/java/com/conversions/conversions/convert/Conversion.java
+++ src/main/java/com/conversions/conversions/convert/Conversion.java
@@ -1,13 +1,35 @@
 package com.conversions.conversions.convert;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class Conversion {
 
-    @RequestMapping("/ktoc")
-    public double KelvinToCelsius(){
-        return 13.23;
+    @RequestMapping(method = RequestMethod.GET, path = "/conversions/ktoc")
+    public Float KelvinToCelsius(@PathParam("kelvin") Float kelvin){
+        Float celsius = kelvin - 273.15F;
+      return celsius;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/conversions/ctok")
+    public Float CelsiusToKelvin(@PathParam("celsius") Float celsius){
+        Float kelvin = celsius + 273.15F;
+        return kelvin;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/conversions/mtok")
+    public Double MilesToKiloMetres(@PathParam("miles") Double miles){
+        Double kilometres = (miles * 1.60934d);
+        return kilometres;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/conversions/ktom")
+    public Double KiloMetresToMiles(@PathParam("miles") Double kilometres){
+        Double miles = (kilometres * 0.621371d);
+        return miles;
     }
 }
